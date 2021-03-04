c641f6d8b00a23db8ad3c473e4b36155baa27652
seilylook@naver.com <seilylook@naver.com>
2021-03-04 14:31:30 +0900

    1 commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const name = "리액트"
+  return(
+    <div
+    style={{
+      backgroundColor: 'black',
+      color: 'aqua',
+      fontSize: '48px',
+      fontWeight: 'bold',
+      padding: 16
+    }}
+    >
+    {name}
+    </div>
+  );
+}
+
+export default App;
