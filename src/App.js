d4375ec658b190073ad05a479889922daf366b36
seilylook@naver.com <seilylook@naver.com>
2021-03-05 13:51:51 +0900

    4 commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import MyComponent from "./MyComponent";
 
 const App = () => {
-  return <MyComponent />;
+  return <MyComponent> REACT </MyComponent>;
 };
 
 export default App;
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const MyComponent = props => {
-  return <div>H!. My name is {props.name} .</div>;
+  const { name, children } = props;
+  return (
+    <div>
+      안녕하세요, 제 이름은 {name} 입니다. <br />
+      children 값은 {children}
+      입니다.
+    </div>
+  );
 };
 
 MyComponent.defaultProps = {
