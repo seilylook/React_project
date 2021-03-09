d6aa1377955b252e32ecec37db3189737f869adf
seilylook@naver.com <seilylook@naver.com>
2021-03-09 20:57:44 +0900

    13 commit

---
+++ src/IterationSample.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const InterationSample = () => {
+  return (
+    <ul>
+      <li>눈사람</li>
+      <li>얼음</li>
+      <li>snow</li>
+      <li>wind</li>
+    </ul>
+  );
+};
+
+export default InterationSample;
